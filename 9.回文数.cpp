017e028948888752d243d306cb8a12dc72f814e2
mahaicheng <mahaicheng@huya.com>
2020-10-09 16:15:19 +0800

    9

--- 9.回文数.cpp
+++ 9.回文数.cpp
@@ -5,11 +5,31 @@
  */
 
 // @lc code=start
+#include<string>
+
+using namespace std;
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-
+        return isPalindrome(to_string(x));
     }
+private:
+    bool isPalindrome(string &&str)
+    {
+        unsigned left = 0;
+        unsigned right = str.size() - 1;
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    };
 };
 // @lc code=end
 

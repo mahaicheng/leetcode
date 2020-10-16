b5f1818ceb0ead798acb0810c8bf7d4f589e5eb3
mahaicheng <mahaicheng@huya.com>
2020-10-16 21:55:10 +0800

    326

---
+++ 326.3-的幂.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=326 lang=cpp
+ *
+ * [326] 3的幂
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
+// @lc code=end

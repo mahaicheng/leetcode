64e74c47336ce557bf366153c1a59bc794ad178e
mahaicheng <mahaicheng@huya.com>
2020-10-26 11:19:37 +0800

    2

--- 2.两数相加.cpp
+++ 2.两数相加.cpp
@@ -15,9 +15,11 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
         if (l1 == nullptr)
         {
             return l2;
@@ -26,7 +28,52 @@ public:
         {
             return l1;
         }
+
+        ListNode *head = nullptr;
+        ListNode *tail = nullptr;
+        int remain = 0;
+        while (l1 != nullptr && l2 != nullptr)
+        {
+            int val = l1->val + l2->val + remain;
+            ListNode *newNode = new ListNode();
+            newNode->val = val % 10;
+            remain = val / 10;
+            if (head == nullptr)
+            {
+                head = newNode;
+                tail = head;
+            }
+            else
+            {
+                tail->next = newNode;
+                tail = tail->next;
+            }
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        if (l1 != nullptr || l2 != nullptr)
+        {
+            ListNode *list = (l1 == nullptr) ? l2 : l1;
+            while (list != nullptr)
+            {
+                int val = list->val + remain;
+                ListNode *newNode = new ListNode();
+                newNode->val = val % 10;
+                remain = val / 10;
+                tail->next = newNode;
+                tail = tail->next;
+                
+                list = list->next;
+            }
+        }
+        if (remain != 0)
+        {
+            ListNode *newNode = new ListNode();
+            newNode->val = remain;
+            tail->next = newNode;
+            tail = newNode;
+        }
+        return head;
     }
 };
 // @lc code=end
-
